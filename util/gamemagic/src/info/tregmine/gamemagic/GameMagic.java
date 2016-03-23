e11908917033285c99bad83aa9dffe34b1fc4a65
Clunker5 <ericjrabil@gmail.com>
2016-03-23 19:58:11 -0400

    Removed check for anarchy world as anarchy is no longer hardocded

--- util/gamemagic/src/info/tregmine/gamemagic/GameMagic.java
+++ util/gamemagic/src/info/tregmine/gamemagic/GameMagic.java
@@ -232,11 +232,6 @@ public class GameMagic extends JavaPlugin implements Listener
     @EventHandler
     public void onBlockBurn(BlockBurnEvent event)
     {
-		if (	event.getBlock().getWorld().getName().equalsIgnoreCase(tregmine.getRulelessWorld().getName()) ||
-				event.getBlock().getWorld().getName().equalsIgnoreCase(tregmine.getRulelessEnd().getName()) ||
-				event.getBlock().getWorld().getName().equalsIgnoreCase(tregmine.getRulelessNether().getName())) {
-			return;
-		}
         event.setCancelled(true);
     }
 
