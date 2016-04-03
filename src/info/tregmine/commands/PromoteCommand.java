625f23781a15de716c76f4b64a30d6e57f78491e
Eric Rabil <ericjrabil@gmail.com>
2016-04-03 16:05:44 -0400

    patched permissions exploit

--- src/info/tregmine/commands/PromoteCommand.java
+++ src/info/tregmine/commands/PromoteCommand.java
@@ -81,6 +81,9 @@ public class PromoteCommand extends AbstractCommand{
 		//Any other errors have now been checked and dealt with. Promote the user.
 		try (IContext ctx = tregmine.createContext()) {
             user.setRank(rank);
+            if(rank != Rank.SENIOR_ADMIN && rank != Rank.GUARDIAN && RANK != Rank.JUNIOR_ADMIN){
+            	user.setStaff(false);
+            }
             user.setMentor(null);
 
             IPlayerDAO playerDAO = ctx.getPlayerDAO();
