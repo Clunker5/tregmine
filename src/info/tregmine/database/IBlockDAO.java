f37d3e1adc19e530b79ea59daa94363a09690174
Eric Rabil <ericjrabil@gmail.com>
2016-04-30 21:20:17 -0400

    loadBlockMinePrices

--- src/info/tregmine/database/IBlockDAO.java
+++ src/info/tregmine/database/IBlockDAO.java
@@ -1,5 +1,8 @@
 package info.tregmine.database;
 
+import java.util.Map;
+
+import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
@@ -9,4 +12,5 @@ import info.tregmine.api.TregminePlayer;
 public interface IBlockDAO {
 	public boolean isPlaced(Block a) throws DAOException;
 	int blockValue(Block a) throws DAOException;
+	Map<Material, Integer> loadBlockMinePrices();
 }
