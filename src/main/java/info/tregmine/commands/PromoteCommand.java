91f4b8843a6ea3478a4ec27be5ab19f862e6303b
Eric Rabil <ericrabil@tregmine.com>
2017-04-02 11:15:40 -0400

    oh my god did i not know java or something

--- src/main/java/info/tregmine/commands/PromoteCommand.java
+++ src/main/java/info/tregmine/commands/PromoteCommand.java
@@ -7,6 +7,7 @@ import info.tregmine.database.DAOException;
 import info.tregmine.database.IContext;
 import info.tregmine.database.IPlayerDAO;
 import net.md_5.bungee.api.chat.TextComponent;
+import org.apache.commons.lang3.StringUtils;
 import org.bukkit.ChatColor;
 import org.bukkit.Server;
 
@@ -24,50 +25,6 @@ public class PromoteCommand extends AbstractCommand {
     }
 
     @Override
-    public boolean handleOther(Server server, String[] args) {
-        if (args.length != 2) {
-            server.getLogger().info("[PROMOTE] You must specify two arguments.");
-            return true;
-        }
-        List<GenericPlayer> candidate = tregmine.matchPlayer(args[0]);
-        if (candidate.size() != 1) {
-            server.getLogger().info(RED + "The player specified was not found. Please try again.");
-            return true;
-        }
-        GenericPlayer user = candidate.get(0);
-
-        String getrank = args[1];
-
-        Rank rank = user.getRank();
-        Rank oldrank = user.getRank();
-        for (Rank r : Rank.values()) {
-            if (r.name().toLowerCase().equals(getrank.toLowerCase())) {
-                rank = r;
-                break;
-            }
-        }
-        try (IContext ctx = tregmine.createContext()) {
-            user.setRank(rank);
-            if (rank != Rank.SENIOR_ADMIN && rank != Rank.GUARDIAN && rank != Rank.JUNIOR_ADMIN) {
-                user.setStaff(false);
-            }
-            user.setMentor(null);
-
-            IPlayerDAO playerDAO = ctx.getPlayerDAO();
-            playerDAO.updatePlayer(user);
-            playerDAO.updatePlayerInfo(user);
-        } catch (DAOException e) {
-            throw new RuntimeException(e);
-        }
-        if (oldrank != rank) {
-            this.tregmine.broadcast(new TextComponent("" + BLUE + ITALIC), user.getChatName(), new TextComponent(
-                    "" + RESET + GREEN + " has been promoted to " + RESET + BLUE + ITALIC + rank + "!"));
-            Tregmine.LOGGER.info(user.getName() + " has been promoted to " + rank);
-        }
-        return true;
-    }
-
-    @Override
     public boolean handlePlayer(GenericPlayer player, String[] args) {
         if (player.getRank() != Rank.SENIOR_ADMIN && !player.isOp()) {
             player.sendMessage(ChatColor.RED + "You certainly don't have permission to promote players!");
@@ -76,51 +33,27 @@ public class PromoteCommand extends AbstractCommand {
         // This player is a senior admin and is allowed to promote. Continue.
         if (args.length != 2) {
             // Player didn't enter two arguments, terminate.
-            player.sendMessage(RED + "You have entered an invalid amount of arguments. Please try again.");
-            return true;
+            return false;
         }
         // The checks have finished, perform the command
-        String possibleuser = args[0];
-        String newrank = args[1];
-        Rank rank = null;
-        String sayrank = "";
-        if (newrank.equalsIgnoreCase("settler")) {
-            rank = Rank.SETTLER;
-            sayrank = "Settler";
-        } else if (newrank.equalsIgnoreCase("resident")) {
-            rank = Rank.RESIDENT;
-            sayrank = "Resident";
-        } else if (newrank.equalsIgnoreCase("donator")) {
-            rank = Rank.DONATOR;
-            sayrank = "Donator";
-        } else if (newrank.equalsIgnoreCase("guardian")) {
-            rank = Rank.GUARDIAN;
-            sayrank = "Guardian";
-        } else if (newrank.equalsIgnoreCase("builder")) {
-            rank = Rank.BUILDER;
-            sayrank = "Builder";
-        } else if (newrank.equalsIgnoreCase("coder")) {
-            rank = Rank.CODER;
-            sayrank = "Coder";
-        } else if (newrank.equalsIgnoreCase("junioradmin") || newrank.equalsIgnoreCase("junior_admin")) {
-            rank = Rank.JUNIOR_ADMIN;
-            sayrank = "Junior Admin";
-        } else if (newrank.equalsIgnoreCase("senioradmin") || newrank.equalsIgnoreCase("senior_admin")) {
-            rank = Rank.SENIOR_ADMIN;
-            sayrank = "Senior Admin";
-        } else {
-            player.sendMessage(RED + "You have specified an invalid rank. Please try again.");
+        Rank rank;
+        try {
+            rank = Rank.valueOf(args[1].toUpperCase());
+        }catch(IllegalArgumentException e){
+            player.sendMessage(RED + "That rank does not exist. Available ranks: ");
+            player.sendMessage(RED + StringUtils.join(Rank.values(), " "));
             return true;
         }
-        List<GenericPlayer> candidate = tregmine.matchPlayer(possibleuser);
+        List<GenericPlayer> candidate = tregmine.matchPlayer(args[0]);
         if (candidate.size() != 1) {
             player.sendMessage(RED + "The player specified was not found. Please try again.");
             return true;
         }
         GenericPlayer user = candidate.get(0);
+        if(rank == user.getRank()){
+            player.sendMessage(RED + "The player already has the desired rank.");
+        }
         if (user.hasFlag(Flags.HARDWARNED)) {
-            // Players with a hardwarn cannot be promoted using this command.
-            // They must be promoted manually.
             player.sendMessage(
                     RED + "The player specified has been hardwarned and is not eligible for promotion.");
             return true;
@@ -141,7 +74,7 @@ public class PromoteCommand extends AbstractCommand {
             throw new RuntimeException(e);
         }
         this.tregmine.broadcast(new TextComponent("" + BLUE + ITALIC), user.getChatName(), new TextComponent(
-                RESET + "" + GREEN + " has been promoted to " + RESET + BLUE + ITALIC + sayrank + "!"));
+                RESET + "" + GREEN + " has been promoted to " + RESET + BLUE + ITALIC + rank.getDiscordEquivalent() + "!"));
         return true;
     }
 
