5e4223bdf5de01c9ebab450c356ad13cd51ff7bd
Eric Rabil <EricRabil@users.noreply.github.com>
2017-07-13 23:27:04 -0400

    Made the clear command great again

--- src/main/java/info/tregmine/commands/CleanInventoryCommand.java
+++ src/main/java/info/tregmine/commands/CleanInventoryCommand.java
@@ -16,22 +16,7 @@ public class CleanInventoryCommand extends AbstractCommand {
             player.sendMessage(ChatColor.RED + "You cannot use that command in this world!");
             return true;
         }
-
-        if (args.length != 1 && !player.hasCommandStatus(CommandStatus.CLEAN_VERIFY)) {
-            player.sendMessage(ChatColor.RED + "Are you sure you want to run that command?");
-            player.sendMessage(ChatColor.RED + "Type /clean yes to confirm");
-            player.setCommandStatus(CommandStatus.CLEAN_VERIFY);
-            return true;
-        } else if (args.length != 1 && player.hasCommandStatus(CommandStatus.CLEAN_VERIFY)) {
-            player.sendMessage(ChatColor.RED + "Type /clean yes to confirm");
-            return true;
-        } else if (args[0].toLowerCase().contains("yes")) {
-            player.removeCommandStatus(CommandStatus.CLEAN_VERIFY);
-            player.sendMessage(ChatColor.AQUA + "Your inventory has been cleared.");
-        } else {
-            this.invalidArguments(player, "/clean <yes>");
-            return true;
-        }
+        player.sendMessage(ChatColor.AQUA + "Your inventory has been cleared.");
         player.getInventory().clear();
         return true;
     }
